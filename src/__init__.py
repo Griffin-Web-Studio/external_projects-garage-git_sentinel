e352a288223801993220d691f038f21fd01c4486
Rihards Simanovics <rihards.s@griffin-web.studio>
2026-06-09 07:41:14 +0000

    refactor: update version number to correct 0.0.0 release
    Also add separation between the const groups

--- src/__init__.py
+++ src/__init__.py
@@ -3,9 +3,11 @@ from pathlib import Path
 
 # ────────────────────────────────────────────────────────────────────| Meta |──
 
-APP_VERSION = "1.0.0"
+APP_VERSION = "0.0.0"
 APP_NAME = "git-sentinel"
+
 CONFIG_DIR = Path.home() / ".config" / APP_NAME
 CONFIG_FILE = CONFIG_DIR / "settings.ini"
+
 STATE_DIR = Path.home() / ".local" / "share" / APP_NAME
 LOCK_FILE = STATE_DIR / "last-run-date"
